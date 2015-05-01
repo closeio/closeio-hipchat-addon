4245c49c1104b0c3651ac664fafdc04737723b57
Phil Freo <phil@philfreo.com>
2015-05-01 20:39:29 +0200

    Changing 'key' name to be in dotted form per instructions on Atlassian's Marketplace "Create add-on" form

--- settings.py
+++ settings.py
@@ -1,6 +1,6 @@
 #SQLALCHEMY_DATABASE_URI = 'sqlite:////tmp/test.db'
 
-HIPCHAT_ADDON_KEY = 'closeio-hipchat'
+HIPCHAT_ADDON_KEY = 'io.close.hipchat-addon'
 HIPCHAT_ADDON_NAME = 'Close.io'
 HIPCHAT_ADDON_DESCRIPTION = 'A HipChat add-on to give details about a Close.io lead when its URL is mentioned in HipChat'
 HIPCHAT_ADDON_VENDOR_URL = 'http://close.io'
